6648b8d988b9e7806c9f0c308d50c1fd20bcf9e7
jakejurado <97064241+jakejurado@users.noreply.github.com>
2022-09-21 15:42:52 -0700

    package.jason and index update

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-const MockQuery = require('./MockQuery_Tools/MockQuery');
-exports.MockQuery = MockQuery;
+// const MockQuery = require('./MockQuery_Tools/MockQuery');
+// exports.MockQuery = MockQuery;
+
+import MockQuery from './MockQuery_Tools/MockQuery';
+export default MockQuery;
